2c7470122770c1939120d57809530ab18469ca22
Will Richanbach <willrichanbach@greeninertia.com>
2023-03-06 16:14:39 -0800

    Completed Vehicle Model List View and Add Model Components

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import VehicleList from './VehicleList';
+import AddModel from './AddModel';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/models/" element={<VehicleList />} />
+          <Route path="/models/new" element={<AddModel />} />
         </Routes>
       </div>
     </BrowserRouter>
