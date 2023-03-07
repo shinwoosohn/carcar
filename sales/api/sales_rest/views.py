cbfce2c0436c530caa9ecfc7e4df5fce95d17670
Will Richanbach <willrichanbach@greeninertia.com>
2023-03-07 12:30:07 -0800

    minor view setup

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 from .models import SalesPerson, Customer, SalesHistory, AutomobileVO
-# Create your views here.
+from common.json import ModelEncoder
+
+
+class SalesPersonEncoder(ModelEncoder):
+    model = SalesPerson
+    properties = ["name", "employee_number", "id"]
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = ["name", "id"]
+
+class SalesHistoryEncoder(ModelEncoder):
+    model = SalesHistory
+    properties = ["seller", "buyer","sale_price", "sold_auto" ]
+
+class AutoEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = ["vin_number", "available_for_sale"]
+
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_sales_persons (request):
+
+
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_customers (request):
+
+
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_saleshistory (request):
+
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_available_autos (request):
